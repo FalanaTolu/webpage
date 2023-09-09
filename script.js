e8e9dd71cd030ff160df90a0e5785df1057d6e01
Tolu Falana <falanaft@gmail.com>
2023-09-09 07:43:08 +0100

    Update UTCTime

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 let day = new Intl.DateTimeFormat("en-US",  { weekday: "long" }).format(new Date());
 document.getElementById('currentDayOfTheWeek').innerHTML = day;
 
-// let utcTime = new Date(Date.UTC());
-// let utcTime = new Date().getTime();
-// let utcTime = new Date().getTime() + new Date().getTimezoneOffset() * 60000;
-var today = new Date().toUTCString();
-document.getElementById('currentUTCTime').innerHTML = today;
-
 let username = document.getElementsByTagName('h1').item(0).innerHTML;
-console.log(username);
-document.getElementsByTagName('img')[0].setAttribute('alt', username)
-console.log(document.getElementsByTagName('img')[0].attributes)
+document.getElementsByTagName('img').item(0).setAttribute('alt', username)
 
 
-const updateClock = () => {
+const updateUTCClock = () => {
     const now = new Date();
-    const clockElement = document.getElementById('clock');
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    setInterval(updateClock, 1000);
+    const utcTime = document.getElementById('currentUTCTime');
+    const hoursUTC = now.getUTCHours().toString().padStart(2, '0');
+    const minutesUTC = now.getUTCMinutes().toString().padStart(2, '0');
+    const secondsUTC = now.getUTCSeconds().toString().padStart(2, '0');
+    setTimeout(updateUTCClock, 1000);
 
-    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    utcTime.textContent = `${hoursUTC}:${minutesUTC}:${secondsUTC}`;
 }
 
-updateClock();
+updateUTCClock();
+
+const updateLocalClock = () => {
+    const now = new Date();
+    const localTime = document.getElementById('clock');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    setTimeout(updateLocalClock, 1000);
+
+    localTime.textContent = `${hours}:${minutes}`;
+}
+
+updateLocalClock();
